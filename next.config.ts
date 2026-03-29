770351d36ea1a1c351d9b7ccf01f61fa115c8461
Neha <nehafiaz12@gmail.com>
2026-03-29 08:37:40 +0500

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  experimental: {
+    viewTransitions: true,
+  },
 };
 
 export default nextConfig;
+
